baf4ba93584be927245ba0db334d7eb53a133e28
nksm <nksm@ness.gr.jp>
2016-08-25 08:57:17 +0900

    2-1 を追加

---
+++ code/2-1_variables.ts
@@ -0,0 +1,15 @@
+let str: string;
+let num: number;
+let bool: boolean;
+
+let func: Function;
+
+let obj: any; // 何でも型
+
+str = "文字列";
+num = 1;
+bool = true;
+func = () => {};
+obj = {};
+
+export {}
